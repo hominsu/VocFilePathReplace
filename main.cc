6f68905efea3a82edcaad77cae38542591c045f2
HominSu <1774069959@qq.com>
2021-06-01 22:40:47 +0800

    完成基础功能

--- main.cc
+++ main.cc
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
   }
 
   // 开始计时
-  auto start = std::chrono::system_clock::now();
+  auto start_time_point = std::chrono::system_clock::now();
 
   std::string filePath = argv[1];
   std::string replace = argv[2];
@@ -46,34 +46,55 @@ int main(int argc, char **argv) {
   size_t size = files.size();
 
   // 初始化线程池
-//  unsigned int core_num = std::thread::hardware_concurrency();
-  unsigned int core_num = 2;
+  unsigned int core_num = std::thread::hardware_concurrency();
+  std::cout << "Using " << core_num - 1 << " Thread To Process Task" << std::endl;
 
   XThreadPool thread_pool;
   thread_pool.InitialThreadPool(core_num - 1);
   thread_pool.Start();
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-  // 添加任务到任务队列
+  // 任务队列
   std::vector<std::shared_ptr<ReplaceTask>> tasks;
+  // 文件切分容器
+  std::vector<std::vector<std::string>> task_files;
 
+  // 文件任务切分
+  for (unsigned int i = 0; i < core_num - 1; ++i) {
+    unsigned int start, end;
+    if (core_num - 2 != i) {
+      start = (size / (core_num - 1)) * i;
+      end = (size / (core_num - 1)) * (i + 1) - 1;
+      task_files.emplace_back(files.begin() + start, files.begin() + end);
+    } else {
+      start = (size / (core_num - 1)) * i;
+      end = size - 1;
+      task_files.emplace_back(files.begin() + start, files.begin() + end);
+    }
+  }
+
+  // 添加任务到任务队列
   for (int i = 0; i < core_num - 1; ++i) {
     auto task = std::make_shared<ReplaceTask>();
     tasks.push_back(task);
     task->replace_str_ = replace;
-    task->files_ = files;
+    task->files_ = task_files[i];
     thread_pool.AddTask(task);
   }
 
   for (int i = 0; i < core_num - 1; ++i) {
     auto ret = tasks[i]->GetReturn();
-    std::cout << "task" << i << ": " << ret << std::endl;
+    if (true == ret) {
+      std::cout << "batch-" << i + 1 << ": succeed" << std::endl;
+    } else {
+      std::cout << "batch-" << i + 1 << ": failed" << std::endl;
+    }
   }
 
   std::cout << "\rDone...          " << std::endl << std::endl;
 
   // 结束计时
-  std::chrono::duration<double> diff = std::chrono::system_clock::now() - start;
+  std::chrono::duration<double> diff = std::chrono::system_clock::now() - start_time_point;
   std::cout << "\tUsed: " << diff.count() << " Second" << std::endl;
 
   return 0;
